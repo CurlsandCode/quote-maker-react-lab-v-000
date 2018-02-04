b3bb18a27b3f0cc3644fcf5cb558cf2380c8a904
Monika Williams <monika.m.williams@gmail.com>
2018-02-04 11:45:43 +0000

    automatically backed up by learn

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -5,21 +5,21 @@ export function addQuote(quote) {
       quote: quote,
     }
   }
-  
+
   export function removeQuote(quoteId) {
     return {
       type: 'REMOVE_QUOTE',
       quoteId: quoteId,
     }
   }
-  
+
   export function upvoteQuote(quoteId) {
     return {
       type: 'UPVOTE_QUOTE',
       quoteId: quoteId,
     }
   }
-  
+
   export function downvoteQuote(quoteId) {
     return {
       type: 'DOWNVOTE_QUOTE',
